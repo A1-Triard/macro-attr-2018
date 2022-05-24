b3342e937b7c81bf15e5f0ddd2f5febc4a395c9b
Warlock <internalmike@gmail.com>
2022-05-24 22:56:53 +0300

    version = major.minor.patch

--- build.rs
+++ build.rs
@@ -1,8 +1,11 @@
 use rustc_version::{Version, version};
+use semver::{BuildMetadata, Prerelease};
 
 fn main() {
-    let version = version().unwrap();
+    let mut version = version().unwrap();
     assert!(version.major >= 1);
+    version.pre = Prerelease::EMPTY;
+    version.build = BuildMetadata::EMPTY;
     if version >= Version::parse("1.62.0").unwrap() {
         println!("cargo:rustc-cfg=rustc_1_62");
     }
