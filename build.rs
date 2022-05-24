65f4822aba2ad71144030c6dff0052667dd0b663
Warlock <internalmike@gmail.com>
2022-05-24 22:34:32 +0300

    fix

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use rustc_version::{Version, version};
 fn main() {
     let version = version().unwrap();
     assert!(version.major >= 1);
-    if version >= Version::parse("1.61.0").unwrap() {
-        println!("cargo:rustc-cfg=rustc_1_61");
+    if version >= Version::parse("1.62.0").unwrap() {
+        println!("cargo:rustc-cfg=rustc_1_62");
     }
 }
