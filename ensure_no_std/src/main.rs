28eb7a52ffd209a08cc8fda3f79217faf03fbdf4
Warlock <internalmike@gmail.com>
2023-07-24 15:46:08 +0300

    ens

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -1,43 +1,19 @@
-#![feature(default_alloc_error_handler)]
 #![feature(start)]
 
 #![deny(warnings)]
 
 #![no_std]
 
-use core::alloc::Layout;
-use core::panic::PanicInfo;
-use macro_attr_2018::macro_attr;
-#[cfg(not(windows))]
-use libc::exit;
-use libc_alloc::LibcAlloc;
-#[cfg(windows)]
-use winapi::shared::minwindef::UINT;
-#[cfg(windows)]
-use winapi::um::processthreadsapi::ExitProcess;
-
 #[cfg(windows)]
 #[link(name="msvcrt")]
 extern { }
 
-#[global_allocator]
-static ALLOCATOR: LibcAlloc = LibcAlloc;
-
-#[cfg(windows)]
-unsafe fn exit(code: UINT) -> ! {
-    ExitProcess(code);
-    loop { }
-}
-
 #[panic_handler]
-pub extern fn panic(_info: &PanicInfo) -> ! {
-    unsafe { exit(99) }
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    exit_no_std::exit(99)
 }
 
-#[no_mangle]
-pub fn rust_oom(_layout: Layout) -> ! {
-    unsafe { exit(98) }
-}
+use macro_attr_2018::macro_attr;
 
 trait TypeName {
     fn type_name() -> &'static str;
